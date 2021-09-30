84c1ecafbda16e687b64a2176a8fe3908f64a021
MarianaAleman <itzel.aleman2016@gmail.com>
2021-09-30 12:05:39 +0100

    First Part
    Just add first screen, and create the second (address)

--- address.js
+++ address.js
@@ -4,16 +4,11 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
+      
     <View style={styles.container}>
-
-        <Image
-          source={require('./img/logo.jpeg')}
-          style={styles.picture}
-        />
-
-      <button >Log In</button>
-
-
+        <div>
+          
+        </div>
     </View>
   );
 }
